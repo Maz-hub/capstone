4b1f8fec7636165a693d80ecb26bd49d281437a5
Maz <mazoolander@me.com>
2025-04-27 12:10:15 +0200

    Fix STATICFILES_DIRS path for Render

--- rivieraroutes/settings.py
+++ rivieraroutes/settings.py
@@ -102,7 +102,7 @@ USE_TZ = True
 # Static files
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 STORAGES = {
     "default": {
